f6bfd638765e48f012c9a3afd11b44adea4f5c63
Peter van de Koolwijk <kooltjes@yahoo.com>
2018-06-09 14:09:01 +0200

    #Features - none
    #Fixes
    -Updated hello

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function helloDeveloper(){
+    console.log('******************************************************\n\n' +
+        'Hello Node Developer,\n\n' +
+        'This is a SDK to connect to ING Bank with NodeJS\n' +
+        'First version will be available in Juli 2018\n' +
+        '\n' +
+        'Please check regually for updates\n\n' +
+        '******************************************************\n\n'
+    );
+}
+
+/**
+ * Welcomes the Developer using the ING-nodejs-sdk
+ * @return {string}
+ */
+module.exports.hello = helloDeveloper;
+
+
+
+
+
+
+
